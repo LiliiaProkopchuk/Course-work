0cdaebfd6b16dc01269dc932e2b4ae797c1524df
Liliia <liliiaprokopchuk@gmail.com>
2020-07-15 01:57:18 +0300

    Version 14.07

---
+++ 14.07/kaktus/assets/js/about.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    //       Menu
+    $(".header_burger").click(function (event) {
+        $(".header_burger, .header_menu").toggleClass("active");
+        $("body").toggleClass("lock");
+    });
+});
